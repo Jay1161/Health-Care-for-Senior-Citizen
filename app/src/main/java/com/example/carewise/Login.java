0e4ca1c153deded3022c43929e5e81667120647b
harmish <harmishsejpal66500@gmail.com>
2023-07-21 12:33:45 +0530

    Error Solve

--- app/src/main/java/com/example/carewise/Login.java
+++ app/src/main/java/com/example/carewise/Login.java
@@ -50,7 +50,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         txtsignup = findViewById(R.id.txtsignup);
