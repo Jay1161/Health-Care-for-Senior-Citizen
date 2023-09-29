9cf6add484b4ca1f448f34c2ce2d5ec01141f9fd
Jay Vaja <87272871+Jay1161@users.noreply.github.com>
2023-09-29 18:35:14 +0530

    Emergency contact features are added.

--- app/src/main/java/com/example/carewise/MainActivity.java
+++ app/src/main/java/com/example/carewise/MainActivity.java
@@ -3,15 +3,19 @@ package com.example.carewise;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.carewise.Fragment.AboutFragment;
 import com.example.carewise.Fragment.DashboardFragment;
@@ -19,6 +23,7 @@ import com.example.carewise.Fragment.HomeFragment;
 import com.example.carewise.Fragment.NotificationFragment;
 import com.example.carewise.Fragment.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,14 +31,20 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView; // Add this line
+    public static final String Shared_PREFS = "sharedPrefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigationView = findViewById(R.id.nav_drawer);
         bottomNavigationView = findViewById(R.id.nav_view);
+
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, new HomeFragment()).commit();
+
         drawerLayout = findViewById(R.id.my_drawer_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
@@ -41,6 +52,37 @@ public class MainActivity extends AppCompatActivity {
 
         // to make the Navigation drawer icon always appear on the action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Handle item clicks here
+                int itemId = item.getItemId(); // Get the selected item's ID
+
+                if (itemId == R.id.theme) {
+                    // Handle the "Theme" item click
+                    Toast.makeText(MainActivity.this, "Theme clicked", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.share) {
+                    // Handle the "Share" item click
+                    Toast.makeText(MainActivity.this, "Share clicked", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.rate_us) {
+                    // Handle the "Rate Us" item click
+                    Toast.makeText(MainActivity.this, "Rate Us clicked", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.about) {
+                    // Handle the "About" item click
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, new AboutFragment()).commit();
+                } else if (itemId == R.id.logout) {
+                    // Handle the "Logout" item click
+
+                    Toast.makeText(MainActivity.this, "Logout Successful", Toast.LENGTH_SHORT).show();
+                }
+
+                // Close the drawer after item click
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
+
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
---
+++ app/src/main/java/com/example/carewise/model/EmergencyContact.java
@@ -0,0 +1,19 @@
+package com.example.carewise.model;
+
+public class EmergencyContact {
+    private String name;
+    private String phoneNumber;
+
+    public EmergencyContact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
